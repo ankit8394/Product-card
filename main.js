22a7463e7e62d2ddb6b62fb51959dd76f0e4c0bb
Ankit <ankit0839483@gmail.com>
2024-09-11 00:37:04 +0530

    add on main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function one(){
-    document.getElementById("")
-}
+function one() {
+    document.getElementById("img1").src = "assets/green.png";
+    document.getElementById("path").style.background="#BCFA2C";
+  }
+  function two() {
+    document.getElementById("img1").src = "assets/blue.png";
+    document.getElementById("path").style.background="#2028F9";
+  }
+  function three() {
+    document.getElementById("img1").src = "assets/red.png";
+    document.getElementById("path").style.background="#FD2A39";
+  }
